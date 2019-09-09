b51f099fe9c7efa146ba9bd3f930d59d61413e87
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-09-09 15:18:40 -0500

    add asp.net core mvc web app with one test.

---
+++ ExampleWebApp/ExampleWebApp.Tests/HomeControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ExampleWebApp.WebApp.Controllers;
+using Xunit;
+
+namespace ExampleWebApp.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void HomeControllerHomeNotNull()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Index();
+
+            Assert.NotNull(result);
+        }
+    }
+}
